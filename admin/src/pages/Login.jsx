120cdd56509ff85cac82f725a1691455849bd5f2
Rajat Tiwari <44523388+rajatreigns@users.noreply.github.com>
2025-04-11 08:58:16 +0530

    Update Login.jsx

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const backendUrl = "const backendUrl"
+  const backendUrl = "https://aumhealthcare-backend.onrender.com"
 
   const { setDToken } = useContext(DoctorContext)
   const { setAToken } = useContext(AdminContext)
