c65ee909deb808d7b066d0fc05e5b888492e3c15
Rajat Tiwari <44523388+rajatreigns@users.noreply.github.com>
2025-05-05 09:06:46 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
           <ul className="flex flex-col gap-2 text-gray-600">
             <li>+91 6204557626</li>
             <li>aumhealthcare@gmail.com</li>
-            <li><a href="https://web.whatsapp.com" target="_blank">Open WhatsApp Web</a></li>
+            <li><a href="https://web.whatsapp.com" target="_blank">Chat With Us on WhatsApp</a></li>
           </ul>
         </div>
       </div>
