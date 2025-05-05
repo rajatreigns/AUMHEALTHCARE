06dfe5d9c323e6b69f776d99266434db2c329eac
Rajat Tiwari <44523388+rajatreigns@users.noreply.github.com>
2025-05-05 09:22:12 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -25,6 +25,16 @@ const Header = () => {
           <img className="w-3" src={assets.arrow_icon} alt="" />
         </a>
       </div>
+        </div>
+        <a
+          href="#speciality"
+          className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-[#595959] text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+        >
+          Book appointment{" "}
+          <img className="w-3" src={assets.arrow_icon} alt="" />
+        </a>
+      </div>
+
 
       {/* --------- Header Right --------- */}
       <div className="md:w-1/2 relative">
